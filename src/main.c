6b0a9e57e069b11cca29ee8e5d36bfac5a3ff56d
Daniel Kukharau <daniel.kukharau@gmail.com>
2016-04-04 04:00:35 +0300

    code cleanup

--- src/main.c
+++ src/main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
-#include <unistd.h>
 #include <errno.h>
 #include <time.h>
 #include "tree.h"
@@ -11,7 +10,9 @@
 
 
 int main(int argc, char *argv[]) {
+
      clock_t start = clock();
+
      if (argc != 4 || !(strcmp(argv[2], "-c") == 0 || strcmp(argv[2], "-x") == 0)) {
           fprintf(stderr, "%s\n\n%s\n%s\n", "Incorrect Usage", "Usage:",
                   "huff input [ -c|-x ] output");
